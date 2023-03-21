fbeae43695be706490018af9ae70f7cdf92f9c4b
Josh Hembree <josh.hembree@rover.com>
2023-03-21 13:13:09 -0700

    update install

--- install.sh
+++ install.sh
@@ -1,18 +1,10 @@
-# ------------------------------------------------------ Exports -------------------------------------------------------
+#!/bin/sh
 
-# ------------------------------------------------------ Aliases -------------------------------------------------------
-
-alias thisBranch="git branch | grep '^\*' | cut -d' ' -f2" #        shows text for current branch of current directory #
-alias darker="python -mdarker --config ./pyproject.toml --isort --revision master... src/aplaceforrover"
-alias prs="gh pr list --author mashdots" #                                               list my current PRs in Github #
-
-# ----------------------------------------------------- Functions ------------------------------------------------------
-
-commit() { # <-- Wrapper for git commit with a message for the current branch
-  local message="$*"
-  if ((${#message} > 0)); then
-    git commit -m "[`thisBranch`] $message"
-  else
-    echo "you need to append a commit message"
-  fi
+zshrc() {
+    echo "==========================================================="
+    echo "                  Import zshrc                             "
+    echo "-----------------------------------------------------------"
+    cat .zshrc > $HOME/.zshrc
 }
+
+zshrc
