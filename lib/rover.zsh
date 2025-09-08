d2a05f8d8a8dcc4b452ae5d4bcd16acf56f7b2e2
Josh Hembree <josh.hembree@rover.com>
2025-09-08 16:02:38 -0700

    move raws check into start

--- lib/rover.zsh
+++ lib/rover.zsh
@@ -7,6 +7,28 @@ export RAWS_USE_DEVICE_CODE=0
 export LAST_RAWS_LOGIN_FILE="$HOME/last_raws_login"
 
 
+function maybe-login-raws() { # <-- Log in to RAWS if it has been more than 24 hours since the last login
+  local NOW=$(date +%s)
+  local LAST_LOGIN=0
+
+  if [[ -f $LAST_RAWS_LOGIN_FILE ]]; then
+    LAST_LOGIN=$(cat $LAST_RAWS_LOGIN_FILE)
+  else
+    echo 0 >| $LAST_RAWS_LOGIN_FILE
+  fi
+
+  local DIFF=$(( (NOW - LAST_LOGIN) / 3600 ))
+
+  if (( DIFF >= 23 )); then
+    printf "\n$fg[green]Logging in to RAWS$reset_color\n"
+    ~/.local/bin/raws profile dev
+    echo $NOW >| $LAST_RAWS_LOGIN_FILE
+  elif (( DIFF >= 20 )); then
+    printf "\n$fg[blue]Last RAWS login was $DIFF hours ago. Log in will be triggered soon$reset_color\n"
+  fi
+}
+
+
 function blacken() { # <-- Wrapper for black
   if [[ -f $1 ]]; then
     black --config ./pyproject.toml $1
@@ -56,6 +78,9 @@ function rtest() { # <-- Wrapper to translate path to Rover test paths
 }
 
 function start() { # <-- Start all docker containers
+  maybe-login-raws
+
+
   until dc up -d
   do
       echo "Trying to start . . ."
@@ -69,27 +94,4 @@ function write_aws_saml_credentials() { # <-- Write AWS SAML credentials to file
         echo "$ROVER_AWS_SAML_HELPER_CREDENTIALS" | base64 -d > "$HOME"/.aws/credentials
     fi
 }
-
-function maybe-login-raws() { # <-- Log in to RAWS if it has been more than 24 hours since the last login
-  local NOW=$(date +%s)
-  local LAST_LOGIN=0
-
-  if [[ -f $LAST_RAWS_LOGIN_FILE ]]; then
-    LAST_LOGIN=$(cat $LAST_RAWS_LOGIN_FILE)
-  else
-    echo 0 >| $LAST_RAWS_LOGIN_FILE
-  fi
-
-  local DIFF=$(( (NOW - LAST_LOGIN) / 3600 ))
-
-  if (( DIFF >= 23 )); then
-    printf "\n$fg[green]Logging in to RAWS$reset_color\n"
-    ~/.local/bin/raws profile dev
-    echo $NOW >| $LAST_RAWS_LOGIN_FILE
-  elif (( DIFF >= 20 )); then
-    printf "\n$fg[blue]Last RAWS login was $DIFF hours ago. Log in will be triggered soon$reset_color\n"
-  fi
-}
-
-maybe-login-raws
 
