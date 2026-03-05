365631ad01033cddbeb2e20b7bbb64035a201cb2
Josh Hembree <josh.hembree@rover.com>
2026-03-05 14:36:55 -0800

    [master] mcp init in web

--- install.sh
+++ install.sh
@@ -55,6 +55,7 @@ setup() {
     echo "-----------------------------------------------------------"
     cp -r lib "$HOME"/.zshrc.d
     cp .zshrc "$HOME"/.zshrc
+    mv /workspaces/.mcp-sample.json /workspaces/.mcp.json
     mkdir "$HOME"/.dotfiles
     chmod 777 "$HOME"/.dotfiles
     cat CLAUDE.md >> /workspaces/web/AGENTS.md
